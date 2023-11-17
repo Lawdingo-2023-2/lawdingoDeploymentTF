04cd0ab51b78ace6b81df5c525664fff2c6dec9b
Camila Diaz <Yhadiraflores109@gmail.com>
2023-11-17 00:31:09 -0500

    modificación del users

--- src/app/component/users/users-listar/users-listar.component.ts
+++ src/app/component/users/users-listar/users-listar.component.ts
@@ -4,6 +4,8 @@ import { Users } from 'src/app/model/users';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { LoginService } from 'src/app/service/login.service';
+import { RoleService } from 'src/app/service/role.service';
+import { Role } from 'src/app/model/role';
 @Component({
   selector: 'app-users-listar',
   templateUrl: './users-listar.component.html',
@@ -14,6 +16,8 @@ export class UsersListarComponent implements OnInit {
     role:string="";
     ///////////////
   dataSource: MatTableDataSource<Users> = new MatTableDataSource();
+  arrUs:Users[]=[]
+  arrRo:Role[]=[]
   displayedColumns: string[] = [
     'idUser',
     'username',
@@ -28,17 +32,21 @@ export class UsersListarComponent implements OnInit {
   ];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private uS: UsersService, private ls:LoginService) {}
+  constructor(private uS: UsersService, private ls:LoginService, private rS:RoleService) {}
   ngOnInit(): void {
         //AGREGACION DE ROL-------------------
         this.role=this.ls.showRole();
-        //////////////
+        this.rS.list().subscribe((data) => {
+       this.arrRo=data;
+        });
     this.uS.list().subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
+      this.filtro(data);
+      this.dataSource = new MatTableDataSource(this.arrUs);
       this.dataSource.paginator = this.paginator;
     });
     this.uS.getList().subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
+      this.filtro(data);
+      this.dataSource = new MatTableDataSource(this.arrUs);
       this.dataSource.paginator = this.paginator;
     });
   }
@@ -54,4 +62,42 @@ export class UsersListarComponent implements OnInit {
   filter(en: any) {
     this.dataSource.filter = en.target.value.trim();
   }
+
+ filtro(data:any){
+    if(this.role == 'ABOGADO'){
+      let roles:Role[]=[];
+      for(let i=0;i<this.arrRo.length;i++){
+        if(this.arrRo[i].rol =='CLIENTE'){
+          roles.push(this.arrRo[i])
+        }
+      };
+      for(let i=0;i<roles.length;i++){
+        for(let j=0;j<data.length;j++){
+        if(data[j].idUser==roles[i].user.idUser){
+          this.arrUs.push(data[j])
+        }
+      };
+    }
+    }
+    else if (this.role == 'CLIENTE'){
+      let roles:Role[]=[];
+      for(let i=0;i<this.arrRo.length;i++){
+        if(this.arrRo[i].rol =='ABOGADO'){
+          roles.push(this.arrRo[i])
+        }
+      };
+      for(let i=0;i<roles.length;i++){
+        for(let j=0;j<data.length;j++){
+        if(data[j].idUser==roles[i].user.idUser){
+          this.arrUs.push(data[j])
+        }
+      };
+
+    }
+    }
+    else{
+      this.arrUs = data;
+    }
+
+}
 }
