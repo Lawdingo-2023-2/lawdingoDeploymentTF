0ba835fceab806cdc0be4bdfa0cb2521d319bcc5
EnzoHe <u202010112@upc.edu.pe>
2023-11-18 03:11:19 -0500

    Query CommentsByLawyer

--- src/app/component/comment/comment-reporte/comment-reporte.component.ts
+++ src/app/component/comment/comment-reporte/comment-reporte.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { CommentByLawyerDTO } from 'src/app/model/CommentByLawyerDTO';
 import { Users } from 'src/app/model/users';
+import { CommentService } from 'src/app/service/comment.service';
+import { UsersService } from 'src/app/service/users.service';
 
 @Component({
   selector: 'app-comment-reporte',
@@ -10,17 +18,47 @@ import { Users } from 'src/app/model/users';
   styleUrls: ['./comment-reporte.component.css'],
 })
 export class CommentReporteComponent implements OnInit {
-  dataSource: MatTableDataSource<CommentByLawyerDTO> = new MatTableDataSource<CommentByLawyerDTO>();
-
+  dataSource: MatTableDataSource<CommentByLawyerDTO> =
+    new MatTableDataSource<CommentByLawyerDTO>();
+  mensaje: string = '';
   form: FormGroup = new FormGroup({});
+  abogado: Users = new Users();
   idAbogadoSeleccionado: number = 0;
   listaUsuarios: Users[] = [];
   listaAbogados: Users[] = [];
-
   displayedColumns: string[] = ['cliente', 'descripcion', 'puntos'];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private cS: CommentService,
+    private lawS: UsersService,
+    private formBuilder: FormBuilder
+  ) {}
+
   ngOnInit(): void {
-      
+    this.form = this.formBuilder.group({
+      lawyer: ['', Validators.required],
+    });
+
+    this.lawS.list().subscribe((data) => {
+      this.listaUsuarios = data;
+
+      this.listaAbogados = this.listaUsuarios.filter((obj) => {
+        return obj.lawyer == true;
+      });
+    });
+  }
+
+  buscar() {
+    if(this.form.valid) {
+      this.abogado.idUser = this.form.value.lawyer;
+      console.log(this.abogado.idUser);
+      this.cS.getcomments(this.abogado.idUser).subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
+      })
+    }
   }
 
 
