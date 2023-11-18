2edd2c5f48f55fa175233628e3fec121744c792c
AngieQuispe <angiequispetito@gmail.com>
2023-11-18 18:26:58 -0500

    Actu2 - Consultas
    Validacion

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const enviroment={
     production:false,
+    //base:"https://lawdingodeployment2023.onrender.com"//direccion del servidor del backend
     base:"http://localhost:8081"//direccion del servidor del backend
 }
