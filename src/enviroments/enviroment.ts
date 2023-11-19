4c8076e076ab000748b99be539932e7cbffd235f
NicolasCarrascoHelfer <116764139+NicolasCarrascoHelfer@users.noreply.github.com>
2023-11-19 01:28:34 -0500

    Update enviroment.ts

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,5 @@
 export const enviroment={
     production:false,
     //base:"https://lawdingodeployment2023.onrender.com"//direccion del servidor del backend
-    base:"https://lawdingobackendtf.onrender.com"
-    //base:"http://localhost:8081"//direccion del servidor del backend
+    base:"http://localhost:8081"//direccion del servidor del backend
 }
