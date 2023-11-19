5c545a6124051a152dac19d719ab03805701092b
NicolasCarrascoHelfer <116764139+NicolasCarrascoHelfer@users.noreply.github.com>
2023-11-19 00:41:15 -0500

    Last

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,6 @@
 export const enviroment={
     production:false,
-    base:"https://lawdingodeployment2023.onrender.com"//direccion del servidor del backend
+    //base:"https://lawdingodeployment2023.onrender.com"//direccion del servidor del backend
+    base:"https://lawdingobackendtf.onrender.com"
     //base:"http://localhost:8081"//direccion del servidor del backend
 }
